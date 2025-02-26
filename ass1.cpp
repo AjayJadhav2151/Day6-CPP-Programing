5b1cdf140398b170fc9185d7d7df4bc3f2dc1baf
AjayJadhav2151 <ajayjadhav.skn.comp@gmail.com>
2025-02-26 20:51:53 +0530

    adding Remaining assignment of Day 6 of CPP programing Assignment number 1

---
+++ ass1.cpp
@@ -0,0 +1,6 @@
+//1.	Write a program to print “Welcome to CDAC” using escape sequences : \n, \t, \r, \.Observe the difference in the output.
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "Welcome to CDAC" << endl;
+}
